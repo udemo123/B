d77d2b7cec7852b59a99c2156acead876cadcacc
udemo123 <demo.user.testing.again@gmail.com>
2024-10-24 11:56:54 +0530

    Update hieraricalclustering.py

--- hieraricalclustering.py
+++ hieraricalclustering.py
@@ -4,9 +4,7 @@ from scipy.cluster.hierarchy import dendrogram, linkage, fcluster
 import matplotlib.pyplot as plt
 
 data = np.array([[85, 90], [80, 85], [75, 80], [60, 70], [90, 95]])
-
 linked = linkage(data, 'ward')
-
 plt.figure(figsize=(10, 7))
 dendrogram(linked)
 plt.title('Hierarchical Clustering Dendrogram')
@@ -14,11 +12,9 @@ plt.xlabel('Data Points')
 plt.ylabel('Distance')
 plt.show()
 
-
 num_clusters = 3 
 df = pd.DataFrame(data, columns=['Feature 1', 'Feature 2']) 
 df['Cluster'] = fcluster(linked, num_clusters, criterion='maxclust')
-
 print("\nData with Cluster Assignments:")
 print(df.head())
 
